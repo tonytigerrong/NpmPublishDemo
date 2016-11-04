eba6cb63125488ec2cab0b0d7a1238ca90189f25
tonyrong <tony@tony.local>
2016-11-03 22:59:27 -0300

    git command test

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ module.exports = function(width, height) {
 
 //this line is added after branch 'feature/2' create in remote and local repo
 
-1st line in feature/2
-2nd line in feature/2
-3nd line in feature/2
+// 1st line in feature/2
+// 2nd line in feature/2
+// 3nd line in feature/2
 
-//this line is added after branch 'feature/1' create in remote and local repo
+// //this line is added after branch 'feature/1' create in remote and local repo
 
-1st line in feature/1
-2nd line in feature/1
-3rd line in feature/1
+// 1st line in feature/1
+// 2nd line in feature/1
+// 3rd line in feature/1
 
  
-//after merge feature/2 to develop
-6th line in develop
-7th line in develop
-https://git-scm.com/book/en/v2/Git-Branching-Rebasing
+// //after merge feature/2 to develop
+// 6th line in develop
+// 7th line in develop
+// https://git-scm.com/book/en/v2/Git-Branching-Rebasing
  
-4rd line in feature/1
+// 4rd line in feature/1
  
